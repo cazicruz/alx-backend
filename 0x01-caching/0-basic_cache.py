6a9e457edc93350ba70d48d8ef21881d94872e9e
david onwuli <dcazicruz@icloud.com>
2023-05-16 20:04:06 +0100

    task0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" caching task """
+
+
+def class BasicCache(BaseCaching):
+    """ceching class that inherits from BaseCeching"""
+    def __init__(self):
+        super.__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            breake
+
+        self.cache_data[key] = item
+    def get(self, key):
+        return self.cache_data.get(key)
