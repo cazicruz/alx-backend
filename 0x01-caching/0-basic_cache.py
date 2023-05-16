dbb277adbb0d8bb4c9d736305053747a8a0d798c
david onwuli <dcazicruz@icloud.com>
2023-05-16 20:15:19 +0100

    task0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,6 +4,7 @@
 
 def class BasicCache(BaseCaching):
     """ceching class that inherits from BaseCeching"""
+    super.__init__():
 
     def put(self, key, item):
         """ Add an item in the cache
@@ -11,6 +12,7 @@ def class BasicCache(BaseCaching):
         if key is None or item is None:
             breake
 
-        self.cache_data[key] = item
+        self.cache_data.update({key:item})
+
     def get(self, key):
         return self.cache_data.get(key)
