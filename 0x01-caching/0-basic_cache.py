d8e6a4d8a8948f7a5980dff997915f7c63820637
david onwuli <dcazicruz@icloud.com>
2023-05-17 03:30:48 +0100

    task0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 """ caching task """
-from typing import Dict, Any
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """ceching class that inherits from BaseCeching"""
-    def __init__(self):
-        super.__init__()
-
-    def put(self, key: str, item: Any):
+    def put(self, key, item):
         """ Add an item in the cache
         """
         if key is None or item is None:
@@ -17,7 +13,7 @@ class BasicCache(BaseCaching):
         else:
             self.cache_data[key] = item
 
-    def get(self, key: str) -> Dict:
+    def get(self, key):
         """ a method to get the value to thr key passed"""
         if key in self.cache_data:
             return self.cache_data[key]
