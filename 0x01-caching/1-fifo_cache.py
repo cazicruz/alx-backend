d2ddd38c3eacc95657f99df3558a2171c7749453
david onwuli <dcazicruz@icloud.com>
2023-05-17 10:19:01 +0100

    task0

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Basic caching module.
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+        self.order_of_entrance = []
+
+
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) > Base.Caching.MAX_ITEMS:
+            for k, value in self.cache_data:
+                if k == self.order_of_entrance[0]:
+                    self.cache_data.pop(k)
+                    self.order_of_entrance.pop(0)
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
