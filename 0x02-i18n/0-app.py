d330378a5b613fdeb218dfbdb4c61763da535d74
david onwuli <dcazicruz@icloud.com>
2023-05-25 22:02:41 +0100

    task0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """ Flask app"""
 from flask import Flask, render_template
-from flask_babel import Babel
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "MYSECRETKEY"
-babal = Babel(app)
+
 
 @app.route('/')
 def index() -> str:
     """ returns the html template for the route"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
