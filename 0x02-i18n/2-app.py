5beb902f7b83f87d8b033ebfa96a991c6bfb5f65
david onwuli <dcazicruz@icloud.com>
2023-05-25 22:27:26 +0100

    task0

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -6,14 +6,15 @@ from flask_babel import Babel
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "MYSECRETKEY"
-basedir = os.path.abspath(os.path.dirname(__file__))
 babal = Babel(app)
 
 class Config(object):
+    SECRET_KEY = "MYSECRETKEY"
     LANGUAGES = ["en", "fr"]
     babel.default_locale='en'
     babel.default_timezone='UTC'
+
+app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
