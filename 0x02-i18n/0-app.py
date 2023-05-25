530b6e4c808ac427584617abc5d1bcc14e97b2ce
david onwuli <dcazicruz@icloud.com>
2023-05-25 06:43:39 +0100

    task0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python3
 """ Flask app"""
-from flask import Flask, render_template, request, session, redirect, url_for,flash
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "MYSECRETKEY"
-basedir = os.path.abspath(os.path.dirname(__file__))
 babal = Babel(app)
-
-class Config(object):
-    LANGUAGES = ["en", "fr"]
-    babel.default_locale='en'
-    babel.default_timezone='UTC'
-
-@babel.localeselector
-def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'[0]])
 
 @app.route('/')
 def index():
